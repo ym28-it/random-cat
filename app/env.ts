ad4b0604cc2f8749739e516bf155c08f4be04991
ym28-it <120317084+ym28-it@users.noreply.github.com>
2025-11-14 15:38:15 +0900

    do not use apt key

--- app/env.ts
+++ app/env.ts
@@ -1,5 +1,6 @@
-if (!process.env.CAT_API_KEY) {
-    throw new Error("Missing env var: CAT_API_KEY");
-}
+// if (!process.env.CAT_API_KEY) {
+//     throw new Error("Missing env var: CAT_API_KEY");
+// }
 
-export const CAT_API_KEY = process.env.CAT_API_KEY;
+// export const CAT_API_KEY = process.env.CAT_API_KEY;
+// export const CAT_API_KEY = DEMO_KEY;
--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -8,9 +8,7 @@ type Image = {
 
 
 export async function fetchImage(): Promise<Image> {
-    const res = await fetch("https://api.thecatapi.com/v1/images/search", {
-        headers: { "x-api-key": CAT_API_KEY },
-    });
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
     const images = await res.json();
     console.log('images', images);
     return images[0];
